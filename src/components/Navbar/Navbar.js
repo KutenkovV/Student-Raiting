669989cdedbccf8608e98eede6925348349ee9f3
KutenkovV <kutenkovvlad017@gmail.com>
2022-04-18 10:13:02 +0800

    Исправил ошибку. Подправил верстку. Добавил атрибуты таблице. но этого мало :С

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import {
   SidebarlistWork,
   SidebarRaitingList,
@@ -9,14 +9,6 @@ import "./Navbar.css";
 function Navbar({active}) { //обернул в {}
   const location = useLocation();
   const isActive = location.pathname;
-
-  //если читаешь этот коммит, то на самом деле
-  //лучше было бы всю логику делать здесь (я про тык на бургер)
-  //но даже так, я всё раво помучился с твоим вариантом и 
-  //много чего нового узнал XD Оказывается, если мы хотим передать
-  //параметры из хука UseState(который юзаем) в другой компонент
-  //то по науке нам нужно использовать в другом компоненте хук UseCallBack
-  //Но как видишь обошлось и без него XD
 
   return (
     <div className={active ? "mainMenu active" : "mainMenu"}>
@@ -29,11 +21,7 @@ function Navbar({active}) { //обернул в {}
               key={index}
               className={item.cName}
               // здесь задаем айдишник с нужным классом если путь айтема равен текущему
-              id={window.location.pathname == item.path ? "active" : ""}
-              // при клике получаем текущий путь
-              onClick={() => {
-                window.location.pathname = item.path;
-              }}
+              id={isActive == item.path ? "active" : ""}
             >
               <Link to={item.path}>
                 <span class="icon">{item.icon}</span>
@@ -50,10 +38,7 @@ function Navbar({active}) { //обернул в {}
             <li
               key={index}
               className={item.cName}
-              id={window.location.pathname == item.path ? "active" : ""}
-              onClick={() => {
-                window.location.pathname = item.path;
-              }}
+              id={isActive == item.path ? "active" : ""}
             >
               <Link to={item.path}>
                 <span class="icon">{item.icon}</span>
@@ -70,10 +55,7 @@ function Navbar({active}) { //обернул в {}
             <li
               key={index}
               className={item.cName}
-              id={window.location.pathname == item.path ? "active" : ""}
-              onClick={() => {
-                window.location.pathname = item.path;
-              }}
+              id={isActive == item.path ? "active" : ""}
             >
               <Link to={item.path}>
                 <span class="icon">{item.icon}</span>
