324daa758dacccabafd6d99d6bf66998eeec25f7
KutenkovV <kutenkovvlad017@gmail.com>
2022-09-18 21:00:47 +0800

    Сделал логику чеклиста. Начал пытаться в скачку Итогового файла

--- client/src/Pages/FinalList.js
+++ client/src/Pages/FinalList.js
@@ -14,12 +14,38 @@ function FinalList() {
       .catch(error => console.log(error));
   }, []);
 
-  return (
-    <div>
-      <h1 className="header">Итоговый список</h1>
-      <FinalTable data={items} itemsPerPage={10} />
-    </div>
-  )
+  //обработка кнопки
+  const onSubmit = async (e) => {
+
+    e.preventDefault();
+
+    //сам пост запрос
+    await axios({
+      method: "GET",
+      url: "http://localhost:8080/api/finalListFile",
+    })
+      .then(() => {
+        console.log("Success!");
+      })
+
+}
+
+return (
+  <div>
+    <h1 className="header">Итоговый список</h1>
+    <FinalTable data={items} itemsPerPage={10} />
+
+
+    {/* Ниже форма с кнопкой которая делает запрос */}
+    <form method="get" action="#" id="#" onSubmit={onSubmit}>
+      <div className="row d-flex justify-content-end">
+        <button class="btn btn-primary col-2 m-4">
+          Скачать список
+        </button>
+      </div>
+    </form>
+  </div>
+)
 }
 
 export default FinalList
