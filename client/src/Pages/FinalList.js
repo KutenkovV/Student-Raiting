a40b21295b86b5443a624550be148f92a55ce3b1
KutenkovV <kutenkovvlad017@gmail.com>
2022-09-24 12:40:47 +0800

    РАБОТАЕТ ЕМАЕ АХАХАХАХ СКАЧИВАНИЕ ФАЙЛА УХАХАХ

--- client/src/Pages/FinalList.js
+++ client/src/Pages/FinalList.js
@@ -3,49 +3,49 @@ import axios from "axios";
 import FinalTable from "../components/tables/FinalTable";
 
 function FinalList() {
-  document.title = "Итоговый список"
+  document.title = "Итоговый список";
   const [items, setItems] = useState([]);
 
 
   //Гет запрос на список
   useEffect(() => {
-    axios.get('http://localhost:8080/api/finalList')
-      .then(response => setItems(response.data))
-      .catch(error => console.log(error));
+    axios
+      .get("http://localhost:8080/api/finalList")
+      .then((response) => setItems(response.data))
+      .catch((error) => console.log(error));
   }, []);
 
   //обработка кнопки
   const onSubmit = async (e) => {
-
     e.preventDefault();
 
     //сам пост запрос
-    await axios({
-      method: "GET",
-      url: "http://localhost:8080/api/finalListFile",
-    })
-      .then(() => {
-        console.log("Success!");
-      })
+    const response = await fetch("http://localhost:8080/api/finalListFile");
+    if (response.status === 200) {
+      const blob = await response.blob();
+      const downloadUrl = window.URL.createObjectURL(blob);
+      const link = document.createElement("a");
+      link.href = downloadUrl;
+      link.download = "Рейтинг";
+      document.body.appendChild(link);
+      link.click();
+      link.remove();
+    }
+  };
 
+  return (
+    <div>
+      <h1 className="header">Итоговый список</h1>
+      <FinalTable data={items} itemsPerPage={10} />
+
+      {/* Ниже форма с кнопкой которая делает запрос */}
+      <form method="get" action="#" id="#" onSubmit={onSubmit}>
+        <div className="row d-flex justify-content-end">
+          <button class="btn btn-primary col-2 m-4">Скачать список</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
-return (
-  <div>
-    <h1 className="header">Итоговый список</h1>
-    <FinalTable data={items} itemsPerPage={10} />
-
-
-    {/* Ниже форма с кнопкой которая делает запрос */}
-    <form method="get" action="#" id="#" onSubmit={onSubmit}>
-      <div className="row d-flex justify-content-end">
-        <button class="btn btn-primary col-2 m-4">
-          Скачать список
-        </button>
-      </div>
-    </form>
-  </div>
-)
-}
-
-export default FinalList
+export default FinalList;
