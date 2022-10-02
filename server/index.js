4a92ccc138f413dfc49c94c4d5ea1881d8f12aa4
Yulia Komogortseva <komog-y-v@yandex.ru>
2022-10-02 20:49:58 +0800

    аккуратно,теперь тут есть авторизация поэтому ничего не работает

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 require("dotenv").config();
-const express = require("express");
+const express = require('express');
 const morgan = require('morgan')
 const errorHandler = require("./middleware/ErrorHandlingMiddleware");
 const cors = require("cors");
 const sequelize = require("./db");
+const basicAuth = require('express-basic-auth')
 const router = require("./routes/routes");
 const fileUpload = require('express-fileupload');
 const PORT = process.env.PORT || 8080;
@@ -11,9 +12,13 @@ const username = process.env.USERNAME;
 const password = process.env.PASSWORD;
 const app = express();
 
+
 const sync = async () => await sequelize.sync({alter:true})
 sync()
 //app.use(express.basicAuth(username,password));
+app.use(basicAuth({
+  users: { username: password }
+}))
 app.use(morgan('tiny'))
 app.use(cors());
 app.use(fileUpload());
