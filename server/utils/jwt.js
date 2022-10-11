b19cdd75802f20c87abb80213ade0866cc7197cd
Yulia Komogortseva <komog-y-v@yandex.ru>
2022-10-11 21:08:53 +0800

    +

--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -1,12 +1,11 @@
 const jwt = require('jsonwebtoken')
 
 module.exports.sign = async (user) => {
-    const JWT_SECRET = 'qemsaslvjd-33r3:9i9vis3.'
     return new Promise((resolve,reject) => {
         jwt.sign({
             username:user.username,
             email: user.email
-        },JWT_SECRET,(err,token) => {
+        },process.env.JWT_SECRET,(err,token) => {
             if(err)
                 return reject(err)
             return resolve(token)
@@ -16,9 +15,8 @@ module.exports.sign = async (user) => {
 }
 
 module.exports.decode = async (token) => {
-    const JWT_SECRET = 'qemsaslvjd-33r3:9i9vis3.'
     return new Promise((resolve,reject) => {
-        jwt.verify(token,JWT_SECRET,(err,decoded) => {
+        jwt.verify(token,process.env.JWT_SECRET,(err,decoded) => {
             if(err)
                 return reject(err)
 
