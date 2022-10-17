e3e04e182357087076c0879ed23d96f9ba114025
KutenkovV <kutenkovvlad017@gmail.com>
2022-10-17 19:38:10 +0800

    Верстка авторизации

--- client/src/Pages/Authorization.js
+++ client/src/Pages/Authorization.js
@@ -1,17 +1,17 @@
 import axios from "axios";
 import React from "react";
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 import { useState } from "react";
 
 const Authorization = ({ setToken }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [msg, setMsg] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+  const [valid, setValid] = useState(false);
 
-  const handleSubmit  = async (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      console.log("сработало!");
       const response = await axios.post(
         "http://localhost:8080/api/users/login",
         {
@@ -25,16 +25,26 @@ const Authorization = ({ setToken }) => {
       // Передаем токен в App.js
       setToken(response.data.user.token);
     } catch (error) {
-      if (error.response) {
-        setMsg(error.response.data.msg);
+      if (error.response?.status === 401) {
+        // Вывод сообщения с сервера
+        setValid(true);
+        setErrMsg(error.response.data.errors.body[1]);
+      } else {
+        setValid(true);
+        setErrMsg("Сервер не отвечает");
       }
     }
   };
 
   return (
     <div className="col-4 container authorizationContainer mt-4">
-      <form onSubmit={handleSubmit }>
-        <p className="has-text-centered">{msg}</p>
+      <form onSubmit={handleSubmit}>
+        <p className="alert alert-primary" role="alert">
+          Для работы в системе авторизуйтесь
+        </p>
+        <p className={valid ? "alert alert-danger" : ""} role="alert">
+          {errMsg}
+        </p>
         <div class="mb-3 m-3">
           <label for="loginInput" class="form-label">
             Логин
@@ -43,7 +53,9 @@ const Authorization = ({ setToken }) => {
             class="form-control"
             id="loginInput"
             value={email}
+            placeholder="Логин"
             onChange={(e) => setEmail(e.target.value)}
+            required
           />
         </div>
         <div class="mb-3 m-3">
@@ -55,7 +67,9 @@ const Authorization = ({ setToken }) => {
             class="form-control"
             id="inputPassword"
             value={password}
+            placeholder="Пароль"
             onChange={(e) => setPassword(e.target.value)}
+            required
           />
         </div>
         <div class="col-auto m-3 d-flex justify-content-end">
