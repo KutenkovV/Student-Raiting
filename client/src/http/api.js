d724661f36c8593c2b9d0042a81790c4a9acffa3
Yulia Komogortseva <komog-y-v@yandex.ru>
2022-10-30 14:32:17 +0800

    fix

--- client/src/http/api.js
+++ client/src/http/api.js
@@ -2,6 +2,9 @@ import axios from 'axios'
 
 export const API_URL = `http://localhost:8080/`
 
+const $api = axios.create({
+    
+})
 
 $api.interceptors.request.use((config) => {
     config.headers.authorization = `Token ${localStorage.getItem('token')}`;
