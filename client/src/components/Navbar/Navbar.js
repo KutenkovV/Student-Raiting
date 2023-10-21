be7bc5bf4ada08b4c8ab48aa7b2dfc49cffcc769
TasyaKh <tasya132002@gmail.com>
2023-10-21 13:19:50 +0800

    feat: сделать кнопку выхода, очищает весь localStorage

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -8,15 +8,18 @@ import "./Navbar.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faTasks,
-  faEdit,
-  faList,
-  faIdCard, faCopy,
+  faSignOut
 } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+import useToken from "../../hooks/useToken";
 
 function Navbar({ active }) {
+  const { token, signOut } = useToken(); // Хук для работы с токеном
+
   //обернул в {}
   const location = useLocation();
   const isActive = location.pathname;
+
 
   return (
     <div className={active ? "mainMenu active-menu" : "mainMenu"}>
@@ -78,6 +81,9 @@ function Navbar({ active }) {
             </li>
           );
         })}
+        <button className="btn btn-secondary m-4 " onClick={() => signOut()}>
+        Выход <span className="mx-1"><FontAwesomeIcon icon={faSignOut} size="1x" /></span>
+      </button>
       </ul>
     </div>
   );
--- client/src/hooks/useToken.js
+++ client/src/hooks/useToken.js
@@ -1,12 +1,17 @@
-import { useState } from 'react';
+import {useState} from 'react';
 
 function useToken() {
 
   // Получаем из локального хранилища наш токен и парсим его в объект
   const getToken = () => {
-    const tokenString = localStorage.getItem('token'); // Ищем по ключу "token"
-    return tokenString; // Возвращаем
+     // Ищем по ключу "token"
+    return localStorage.getItem('token'); // Возвращаем
   };
+
+  const signOut = () =>{
+    localStorage.clear();
+    window.location.reload();
+  }
 
   // Помещаем в стейт наш текущей токен (т.е. вызываем его методом)
   const [token, setToken] = useState(getToken());
@@ -16,11 +21,13 @@ function useToken() {
     localStorage.setItem('token', userToken);
     setToken(userToken);
   };
-  
+
+
   // Возвращаем значения
   return {
     setToken: saveToken,
-    token
+    token,
+    signOut
   }
 }
 
